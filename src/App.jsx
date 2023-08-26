fa951d183784c70784aff058d061ff73fe17c510
TheSh4h <hh.haseebshah@gmail.com>
2023-08-25 23:29:50 -0700

    Layout setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [dark , setDark] = useState(false);
+  const doubleNumber = slowFunction(number)
+  const themeStyles = {
+    backgroundColor: dark ? 'black' : 'white',
+    color: dark ? 'white' : 'black'
+  }
 
   return (
     <>
-      <h1>Hello World</h1>
+      <input type="number" value={number} onChange={e => setNumber(parseInt(e.target.value))} /> <br />
+      <button onClick={() => setDark(prevDark => !prevDark)}>Change Theme</button>
+      <div style={themeStyles}>{doubleNumber}</div>
     </>
   )
+}
+
+function slowFunction(num) {
+  console.log('Calling slowFunction')
+  for (let i = 0; i <= 1000000000; i++) {}
+  return num * 2
 }
 
 export default App
