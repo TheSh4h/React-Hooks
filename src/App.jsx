edf2ccaf8c385aa18af4c184dd23baa45b4c028e
TheSh4h <hh.haseebshah@gmail.com>
2023-08-25 23:47:28 -0700

    useMemo implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
+import { useMemo } from 'react';
 import './App.css'
 
 function App() {
   const [number, setNumber] = useState(0);
   const [dark , setDark] = useState(false);
-  const doubleNumber = slowFunction(number)
+  const doubleNumber = useMemo(() => {
+    return slowFunction(number)
+  }, [number]) 
   const themeStyles = {
     backgroundColor: dark ? 'black' : 'white',
     color: dark ? 'white' : 'black'
