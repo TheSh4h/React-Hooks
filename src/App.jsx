e0fa9da45512e8b743264aae42e48bcf0d17b677
TheSh4h <hh.haseebshah@gmail.com>
2023-08-26 22:47:10 -0700

    focus with ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
+import { useRef } from 'react'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
   const [name, setName] = useState('')
+  const inputRef = useRef()
+
+  function focus() {
+    inputRef.current.focus()
+  }
 
   return (
     <>
-      <input value={name} onChange={e => setName(e.target.value)} />
+      <input ref={inputRef} value={name} onChange={e => setName(e.target.value)} />
       <div>My name is {name}</div>
+      <button onClick={focus}>Focus</button>
     </>
   )
 }
