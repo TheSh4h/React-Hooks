491ddfed50712c97c64cc8f2e101f58d20e73f20
TheSh4h <hh.haseebshah@gmail.com>
2023-08-26 22:30:03 -0700

    layout setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [name, setName] = useState('')
+
   return (
     <>
+      <input value={name} onChange={e => setName(e.target.value)} />
+      <div>My name is {name}</div>
     </>
   )
 }
