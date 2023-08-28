40b24d134cb4a2de99351d7012c07224c7356864
TheSh4h <hh.haseebshah@gmail.com>
2023-08-27 17:30:39 -0700

    layout setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-import { useState, useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
-  const [name, setName] = useState('')
-  const prevName = useRef('')
-
-  useEffect(() => {
-    prevName.current = name
-  }, [name])
 
   return (
     <>
-      <input value={name} onChange={e => setName(e.target.value)} />
-      <div>My name is {name} and it used to be {prevName.current}</div>
     </>
   )
 }
